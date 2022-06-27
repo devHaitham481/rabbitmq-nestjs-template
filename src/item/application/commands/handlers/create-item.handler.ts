b4e701510667b85ff73d15d1a6936f8748d249b2
devHaitham481 <51095418+devHaitham481@users.noreply.github.com>
2022-06-28 00:51:35 +0200

    Initialize feat-#21/create-typeorm-config-service

---
+++ src/item/application/commands/handlers/create-item.handler.ts
@@ -0,0 +1,46 @@
+import { CommandHandler, EventPublisher, ICommandHandler } from '@nestjs/cqrs';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { ErrorValidationService, RpcExceptionService } from '../../../../utils';
+import { ItemEntity } from '../../../infrastructure/entities';
+import { ItemRepository } from '../../../domain/repositories';
+import { ItemWriteRepository } from '../../../infrastructure/repositories';
+import { CreateItemCommand } from '../impl';
+
+@CommandHandler(CreateItemCommand)
+export class CreateItemHandler implements ICommandHandler<CreateItemCommand> {
+  constructor(
+    @InjectRepository(ItemWriteRepository)
+    private readonly itemWriteRepository: ItemWriteRepository,
+    private readonly itemRepository: ItemRepository,
+    private readonly publisher: EventPublisher,
+    private readonly rpcExceptionService: RpcExceptionService,
+    private readonly errorValidationService: ErrorValidationService,
+  ) {}
+
+  async execute(command: CreateItemCommand): Promise<ItemEntity> {
+    const { createItemDto } = command;
+
+    const item = this.itemWriteRepository.create();
+
+    item.name = createItemDto.name;
+
+    try {
+      await item.save();
+
+      const itemModel = this.publisher.mergeObjectContext(
+        await this.itemRepository.createItem(createItemDto),
+      );
+
+      itemModel.commit();
+
+      return item;
+    } catch (error) {
+      const errorObject = this.errorValidationService.validateDbError(
+        error.code,
+      );
+
+      this.rpcExceptionService.throwCatchedException(errorObject);
+    }
+  }
+}
